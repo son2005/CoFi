2f4df76c17b6889fd942e122dc9941a634aaa50f
son2005 <tranbaoson2005@gmail.com>
2017-07-06 13:50:03 -0400

    Add solution for Arcade/TheCore SeatsInTheater

---
+++ Arcade/The Core/4-seats-in-theater.cpp
@@ -0,0 +1,4 @@
+// https://codefights.com/arcade/code-arcade/intro-gates/bszFiQAog96G9CXKg
+int seatsInTheater(int nCols, int nRows, int col, int row) {
+    return (nCols - col + 1) * (nRows - row);
+}
