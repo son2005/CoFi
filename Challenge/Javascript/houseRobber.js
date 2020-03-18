3cb671236928f9104ac3a46e43fe4f73c4a3ccce
son2005 <tranbaoson2005@gmail.com>
2020-03-18 00:48:31 -0400

    shorter

--- Challenge/Javascript/houseRobber.js
+++ Challenge/Javascript/houseRobber.js
@@ -5,6 +5,6 @@ o = e = 0
 // m = (a,b) => a > b ? a : b
 // houseRobber = n => n.map((v,i) => i & 1 ? e = m(e + v, o) : o = m(o + v, e)) | m(e, o)
 
-// 80 chars
-m = () => o > e ? o : e
+// 79 chars
+m = _ => o > e ? o : e
 houseRobber = n => n.map((v,i) => i & 1 ? (e += v, e = m()) : (o += v, o = m())) | m()
